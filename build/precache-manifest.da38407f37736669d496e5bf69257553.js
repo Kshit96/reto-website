f396f4921b6117a353019291106fee655178c031
kshitiz <kshit96@gmail.com>
2020-09-05 17:07:44 +0530

    Create build folder

---
+++ build/precache-manifest.da38407f37736669d496e5bf69257553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960cf44f7c0bfcd6c5caecc1351cb8b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50e9686102c465fb8c89",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a2f0bf8ac0a85df11b5e",
+    "url": "/static/js/2.9970051c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9970051c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e9686102c465fb8c89",
+    "url": "/static/js/main.411975e9.chunk.js"
+  },
+  {
+    "revision": "17fa2faa0218120d94c7",
+    "url": "/static/js/runtime-main.e037b460.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
