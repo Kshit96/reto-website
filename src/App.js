0c4300c11779112ebab382b435660f960fb43b07
kshitiz <kshitiz@greyamp.com>
2020-12-20 05:02:50 +0530

    Add snap scrolling and types of containers in atoms

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import {ReactComponent as Background} from './assets/image.svg';
 import './App.css';
 import {Container} from "react-bootstrap";
+import {
+  StyledDivBlue,
+  StyledDivOrange,
+  StyledDivWhite, StyledDivYellow
+} from "./atoms/Containers";
+import styled from "styled-components";
+
+const StyledContainer = styled(Container)`
+scroll-snap-type: y mandatory;
+  overflow: auto;
+  height: 100vh;
+  width: 100vw;
+`
 
 function App() {
   return (
-    <Container className="App mx-0 my-0" fluid>
-     <Background className={'background'}/>
-     {/*viewBox={'0 0 3840 2180'}*/}
-    </Container>
+      <StyledContainer className={'mx-0 px-0 my-0 py-0'} fluid>
+        <StyledDivBlue><TagLine/></StyledDivBlue>
+        <StyledDivWhite>b</StyledDivWhite>
+        <StyledDivOrange>c</StyledDivOrange>
+        <StyledDivYellow>d</StyledDivYellow>
+      </StyledContainer>
   );
 }
 
---
+++ src/atoms/Containers.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledDivBlue = styled.div`
+height: 100vh;
+width:100vw;
+background-color: #1e3d59;
+scroll-snap-align: start;
+scroll-snap-align: start;
+
+`
+
+export const StyledDivWhite = styled.div`
+height: 100vh;
+width:100vw;
+background-color: #f5f0e1;
+scroll-snap-align: start;
+`
+
+export const StyledDivOrange = styled.div`
+height: 100vh;
+width:100vw;
+background-color: #ff6e40;
+scroll-snap-align: start;
+`
+
+export const StyledDivYellow = styled.div`
+height: 100vh;
+width:100vw;
+background-color: #ffc13b;
+scroll-snap-align: start;
+`
