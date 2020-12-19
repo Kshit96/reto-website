1d5854b26fbb0b7bca8140cc3a0b5159f058104d
kshitiz <kshitiz@greyamp.com>
2020-12-20 02:28:25 +0530

    Try more ways to align the background image in the website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import {ReactComponent as Background} from './assets/image.svg';
 import './App.css';
+import {Container} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App mx-0 my-0" fluid>
+     <Background className={'background'}/>
+     {/*viewBox={'0 0 3840 2180'}*/}
+    </Container>
   );
 }
 
